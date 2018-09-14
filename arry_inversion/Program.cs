f422e0fa1b2c53e2b90e12b1c5326e766f02f791
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-14 13:50:15 +0300

    first commit

---
+++ arry_inversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arry_inversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
