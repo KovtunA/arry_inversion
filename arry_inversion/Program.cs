366d03dfb65994f11fd4c17a69855970a6d4d19e
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-14 16:24:35 +0300

    clear code

--- arry_inversion/Program.cs
+++ arry_inversion/Program.cs
@@ -6,16 +6,12 @@ namespace arry_inversion
     {
         static void Main(string[] args)
         {
-
             int[] arr = new int[10];
 
-            for (int i = 0; i < arr.Length; i++)
-            {
+            for (int i = 0; i < arr.Length; i++) {
                 arr[i] = (new Random()).Next(1000) - 500;
             }
 
-            //var str = string.Join("\n ", arr);
-            //Console.WriteLine($"Random array:\n [{str}]");
             Console.WriteLine("Random array:\n");
             printArray(arr);
 
@@ -23,17 +19,11 @@ namespace arry_inversion
             Array.Reverse(arr);
             printArray(arr);
 
-            //foreach (int value in arr)    {Console.WriteLine(value);}
-
             Console.WriteLine("\nInverted array with function: \n ");
-
             printArray(reverse(arr));
-           
-            //foreach (int value in reverse(arr))  { Console.WriteLine(value); }
-
         }
 
-        static int[] reverse(int[] array) 
+        static int[] reverse(int[] array)
         {
             var res = new int[array.Length];
             for (int i = array.Length - 1; i >= 0; i--) { res[i] = array[array.Length - 1 - i]; }
@@ -42,9 +32,7 @@ namespace arry_inversion
 
         static void printArray(int[] massive)
         {
-            foreach (int value in massive) {
-                Console.WriteLine(value);
-            }
+            foreach (int value in massive) { Console.WriteLine(value); }
         }
     }
 }
