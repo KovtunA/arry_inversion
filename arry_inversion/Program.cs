e7a7165428c08fb91f8aea269b7c032571050050
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-17 17:51:59 +0300

    Extract randomising array in a function

--- arry_inversion/Program.cs
+++ arry_inversion/Program.cs
@@ -7,11 +7,7 @@ namespace arry_inversion
         static void Main(string[] args)
         {
             int[] arr = new int[10];
-            Random rand = new Random();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = rand.Next(1000) - 500;
-            }
+            fillArrayWithRandomNumbers(arr);
 
             Console.WriteLine("Random array:\n");
             printArray(arr);
@@ -29,6 +25,16 @@ namespace arry_inversion
             var res = new int[array.Length];
             for (int i = array.Length - 1; i >= 0; i--) { res[i] = array[array.Length - 1 - i]; }
             return res;
+        }
+
+        static void fillArrayWithRandomNumbers(int[] arr)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1000) - 500;
+            }
         }
 
         static void printArray(int[] massive)
